efd0026286e48c2835917b46eb68ed3fded399e4
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2024-03-09 20:49:27 +0700

    init project

---
+++ src/main/java/com/tma/oop/sharing/OopSharingApplication.java
@@ -0,0 +1,13 @@
+package com.tma.oop.sharing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OopSharingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OopSharingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tma/oop/sharing/OopSharingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tma.oop.sharing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopSharingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
